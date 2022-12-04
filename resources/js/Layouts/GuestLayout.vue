98a186480927f95d6639f27450b6e56cbb68fe26
Olang Daniel <olangdan17@gmail.com>
2022-12-04 11:20:13 +0100

    first commit

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,33 @@
+
+<template>
+
+    <div>
+        <div id="root" class="min-h-100vh flex grow bg-slate-50 dark:bg-navy-900" x-cloak>
+            <main class="grid w-full grow grid-cols-1 place-items-center">
+                <div class="w-full max-w-[26rem] p-4 sm:px-5">
+                    <div class="text-center">
+                        <div>
+                            <Link href="/">
+                                <ApplicationLogo class="mx-auto w-20 h-20 fill-current text-gray-500" />
+                            </Link>
+                        </div>
+                        <div class="mt-4">
+                            <h2 class="text-2xl font-semibold text-slate-600 dark:text-navy-100">
+                                Welcome
+                            </h2>                        
+                        </div>
+                    </div>
+
+                    <slot />
+
+                </div>
+            </main>
+        </div>
+         
+    </div>
+</template>
+
+<script setup>
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- CSS Assets -->
+    <link rel="stylesheet" href="{{ asset('dashboard/css/app.css') }}" />
+
+    <!-- Javascript Assets -->
+    <script src="{{ asset('dashboard/js/app.js') }}" defer></script>
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased is-header-blur" x-data x-bind="$store.global.documentBody">
+    <!-- App preloader-->
+    <div class="app-preloader fixed z-50 grid h-full w-full place-content-center bg-slate-50 dark:bg-navy-900">
+        <div class="app-preloader-inner relative inline-block h-48 w-48"></div>
+    </div>
+    @inertia
+
+    <div id="x-teleport-target"></div>
+    <script>
+        window.addEventListener("DOMContentLoaded", () => Alpine.start());
+    </script>
+</body>
+
+</html>
